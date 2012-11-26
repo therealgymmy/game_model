b6467de59b08100ac8b934ac274fea757efd2b64
omniholy <gnuhcsnilloc912@hotmail.com>
2012-11-25 20:35:40 -0500

    Changing ut_IdStack to work with logging utils

--- test/test_suites/ut_IdStack.c
+++ test/test_suites/ut_IdStack.c
@@ -1,8 +1,9 @@
 #include "../_include/ut_IdStack.h"
 
 // Dependencies
-#include <stdio.h>
-#include <stdbool.h>
+#include "../_include/test_Log.h"
+
+// Required by test
 #include "../../src/model/_include/identity.h"
 
 UnitTestIdStack ut_IdStack = {
@@ -14,103 +15,110 @@ UnitTestIdStack ut_IdStack = {
 
 //---Prototypes---
 
-static bool singleIdStackTest();
-static bool doubleIdStackTest();
+static TestStatus singleIdStackTest();
+static TestStatus doubleIdStackTest();
 
 
 
 //---Implementations---
-//
+
 TestStatus run_idstack() {
     if (!singleIdStackTest()) {
-        printf("Failed singleIdStackTest first try\n");
-        return FAIL;
+        __log_print("Failed singleIdStackTest first try\n");
+        __return_status(FAIL);
     }
     if (!singleIdStackTest()) {
-        printf("Failed singleIdStackTest second try\n");
-        return FAIL;
+        __log_print("Failed singleIdStackTest second try\n");
+        __return_status(FAIL);
     }
 
     if (!doubleIdStackTest()) {
-        printf("Failed doubleIdStackTest first try\n");
-        return FAIL;
+        __log_print("Failed doubleIdStackTest first try\n");
+        __return_status(FAIL);
     }
     if (!doubleIdStackTest()) {
-        printf("Failed doubleIdStackTest second try\n");
-        return FAIL;
+        __log_print("Failed doubleIdStackTest second try\n");
+        __return_status(FAIL);
     }
 
-    return PASS;
+    __return_status(PASS);
 }
 
-bool singleIdStackTest() {
+TestStatus singleIdStackTest() {
     Identity id;
     IdStack* s1 = newIdStack();
     if (!s1) {
-        printf("ERROR:  Function newIdStack() returned NULL pointer\n");
-        return false;
+        __log_print("ERROR:  Function newIdStack() returned NULL pointer\n");
+        __return_status(FAIL);
     }
     if (!s1->idList_) {
-        printf("ERROR:  Function newIdStack() didn't initialize the idList_ pointer\n");
+        __log_print("ERROR:  Function newIdStack() didn't initialize the idList_ pointer\n");
+        __return_status(FAIL);
     }
 
     for (int i = 0; i < MAX_ID_STACK_ID_COUNT; i++) {
         id = newId(s1);
         if (id == NULL_ID) {
-            printf("ERROR:  Function newId() returned null id when still available ids\n");
+            __log_print("ERROR:  Function newId() returned null id when still available ids\n");
+            __return_status(FAIL);
         }
         if (!s1->idList_) {
-            printf("ERROR:  Function newId() didn't properly reallocate the idList_ pointer\n");
+            __log_print("ERROR:  Function newId() didn't properly reallocate the idList_ pointer\n");
+            __return_status(FAIL);
         }
     }
     id = newId(s1);
     if (id != NULL_ID) {
-        printf("ERROR:  Function newId() didn't return null id when max id has been reached\n");
+        __log_print("ERROR:  Function newId() didn't return null id when max id has been reached\n");
+        __return_status(FAIL);
     }
 
     if (!s1->idList_) {
-        printf("ERROR:  The idList_ pointer is NULL prior to deleting the idStack\n");
+        __log_print("ERROR:  The idList_ pointer is NULL prior to deleting the idStack\n");
+        __return_status(FAIL);
     }
     if (!delIdStack(s1)) {
-        printf("ERROR:  Function delIdStack() failed to free valid IdStack pointer\n");
-        return false;
+        __log_print("ERROR:  Function delIdStack() failed to free valid IdStack pointer\n");
+        __return_status(FAIL);
     }
     s1 = NULL;
 
     if (delIdStack(s1)) {
-        printf("ERROR:  Function delIdStack() attempted to free invalid IdStack pointer\n");
-        return false;
+        __log_print("ERROR:  Function delIdStack() attempted to free invalid IdStack pointer\n");
+        __return_status(FAIL);
     }
 
-    return true;
+    __return_status(PASS);
 }
 
-bool doubleIdStackTest() {
+TestStatus doubleIdStackTest() {
     Identity id;
     IdStack* s1 = NULL;
     IdStack* s2 = NULL;
 
     if (delIdStack(s1)) {
-        printf("ERROR:  Function delIdStack() attempted to free invalid IdStack pointer\n");
-        return false;
+        __log_print("ERROR:  Function delIdStack() attempted to free invalid IdStack pointer\n");
+        __return_status(FAIL);
     }
 
     s1 = newIdStack();
     if (!s1) {
-        printf("ERROR:  Function newIdStack() returned NULL pointer\n");
-        return false;
+        __log_print("ERROR:  Function newIdStack() returned NULL pointer\n");
+        __return_status(FAIL);
     }
     if (!s1->idList_) {
-        printf("ERROR:  Function newIdStack() didn't initialize the idList_ pointer\n");
+        __log_print("ERROR:  Function newIdStack() didn't initialize the idList_ pointer\n");
+        __return_status(FAIL);
     }
 
     s2 = newIdStack();
     if (!s2) {
-        printf("ERROR:  Function newIdStack() returned NULL pointer\n");
-        return false;
+        __log_print("ERROR:  Function newIdStack() returned NULL pointer\n");
+        __return_status(FAIL);
     }
     if (!s2->idList_) {
-        printf("ERROR:  Function newIdStack() didn't initialize the idList_ pointer\n");
+        __log_print("ERROR:  Function newIdStack() didn't initialize the idList_ pointer\n");
+        __return_status(FAIL);
     }
 
 
@@ -118,56 +126,64 @@ bool doubleIdStackTest() {
     for (int i = 0; i < MAX_ID_STACK_ID_COUNT; i++) {
         id = newId(s1);
         if (id == NULL_ID) {
-            printf("ERROR:  Function newId() returned null id when still available ids\n");
+            __log_print("ERROR:  Function newId() returned null id when still available ids\n");
+            __return_status(FAIL);
         }
         if (!s1->idList_) {
-            printf("ERROR:  Function newId() didn't properly reallocate the idList_ pointer\n");
+            __log_print("ERROR:  Function newId() didn't properly reallocate the idList_ pointer\n");
+            __return_status(FAIL);
         }
     }
     id = newId(s1);
     if (id != NULL_ID) {
-        printf("ERROR:  Function newId() didn't return null id when max id has been reached\n");
+        __log_print("ERROR:  Function newId() didn't return null id when max id has been reached\n");
+        __return_status(FAIL);
     }
 
     for (int i = 0; i < MAX_ID_STACK_ID_COUNT; i++) {
         id = newId(s2);
         if (id == NULL_ID) {
-            printf("ERROR:  Function newId() returned null id when still available ids\n");
+            __log_print("ERROR:  Function newId() returned null id when still available ids\n");
+            __return_status(FAIL);
         }
         if (!s2->idList_) {
-            printf("ERROR:  Function newId() didn't properly reallocate the idList_ pointer\n");
+            __log_print("ERROR:  Function newId() didn't properly reallocate the idList_ pointer\n");
+            __return_status(FAIL);
         }
     }
     id = newId(s2);
     if (id != NULL_ID) {
-        printf("ERROR:  Function newId() didn't return null id when max id has been reached\n");
+        __log_print("ERROR:  Function newId() didn't return null id when max id has been reached\n");
+        __return_status(FAIL);
     }
 
     if (!s1->idList_) {
-        printf("ERROR:  The idList_ pointer is NULL prior to deleting the idStack\n");
+        __log_print("ERROR:  The idList_ pointer is NULL prior to deleting the idStack\n");
+        __return_status(FAIL);
     }
     if (!delIdStack(s1)) {
-        printf("ERROR:  Function delIdStack() failed to free valid IdStack pointer\n");
-        return false;
+        __log_print("ERROR:  Function delIdStack() failed to free valid IdStack pointer\n");
+        __return_status(FAIL);
     }
     s1 = NULL;
     if (delIdStack(s1)) {
-        printf("ERROR:  Function delIdStack() attempted to free invalid IdStack pointer\n");
-        return false;
+        __log_print("ERROR:  Function delIdStack() attempted to free invalid IdStack pointer\n");
+        __return_status(FAIL);
     }
 
     if (!s2->idList_) {
-        printf("ERROR:  The idList_ pointer is NULL prior to deleting the idStack\n");
+        __log_print("ERROR:  The idList_ pointer is NULL prior to deleting the idStack\n");
+        __return_status(FAIL);
     }
     if (!delIdStack(s2)) {
-        printf("ERROR:  Function delIdStack() failed to free valid IdStack pointer\n");
-        return false;
+        __log_print("ERROR:  Function delIdStack() failed to free valid IdStack pointer\n");
+        __return_status(FAIL);
     }
     s2 = NULL;
     if (delIdStack(s2)) {
-        printf("ERROR:  Function delIdStack() attempted to free invalid IdStack pointer\n");
-        return false;
+        __log_print("ERROR:  Function delIdStack() attempted to free invalid IdStack pointer\n");
+        __return_status(FAIL);
     }
 
-    return true;
+    __return_status(PASS);
 }
